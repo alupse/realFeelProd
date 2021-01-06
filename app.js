f95bfd4ab13bfda34836aa07b18740733b6bf55f
alupse <andreealupse26@gmail.com>
2021-01-06 13:52:59 +0200

    Refactored the payment implementation6

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post(
   bodyParser.raw({ type: '*/*' }),
   bookingController.webhookCheckout
 );
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -63,7 +63,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 //1) GLOBAL Middlewares
 
